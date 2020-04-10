bb969728172b6b44fe157acd269240da17dfa83a
Igor <igor.apfentaler@gmail.com>
2020-04-10 15:25:47 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/style/style.scss";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="main__container">
+        <div className="main__container--sidebar">
+          <ul>
+            <li className="person">
+              <div className="person__in--chat">
+                <img
+                  className="person__image"
+                  src="https://placeimg.com/40/40/people"
+                ></img>
+                <p className="person__info">Ime Prezime</p>
+              </div>
+            </li>
+            <li className="person">
+              <div className="person__in--chat">
+                <img
+                  className="person__image"
+                  src="https://placeimg.com/40/40/people"
+                ></img>
+                <p className="person__info">Ime Prezime</p>
+              </div>
+            </li>
+            <li className="person">
+              <div className="person__in--chat">
+                <img
+                  className="person__image"
+                  src="https://placeimg.com/40/40/people"
+                ></img>
+                <p className="person__info">Ime Prezime</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="main__container--content">sddsgf</div>
+      </div>
     </div>
   );
 }
